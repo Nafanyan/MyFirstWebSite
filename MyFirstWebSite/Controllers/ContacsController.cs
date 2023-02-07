50e0b611ee961e33bb521f8a8a9e474a58be8d95
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-02-07 17:38:29 +0300

    Add project files.

---
+++ MyFirstWebSite/Controllers/ContacsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebSite.Models;
+
+namespace MyFirstWebSite.Controllers
+{
+    public class ContacsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(ContactsModel contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
